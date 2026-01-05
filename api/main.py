7ea836c347fff4da233be309ea51d69360b6dc69
omoinjm <njmcloud@gmail.com>
2026-01-05 12:22:33 +0200

    add favicon

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse,  HTMLResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 from .routers import upload
 from .middleware.error_handling_middleware import ErrorHandlingMiddleware
 
@@ -9,12 +9,14 @@ app = FastAPI()
 
 app.add_middleware(ErrorHandlingMiddleware)
 
+
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
         status_code=422,
         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
     )
+
 
 @app.get("/", response_class=HTMLResponse)
 def read_root():
@@ -325,8 +327,10 @@ def read_root():
     </html>
     """
 
+
 @app.get("/health")
 async def health_check():
     return JSONResponse(status_code=200, content={"status": "ok"})
+
 
 app.include_router(upload.router, prefix="/api/v1")
