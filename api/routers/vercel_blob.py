aa5c6cbd813a9854fd849a5b026e27678f1280e2
omoinjm <njmcloud@gmail.com>
2026-01-05 14:31:24 +0200

    idk wahgwaan with the favicon

--- api/routers/vercel_blob.py
+++ api/routers/vercel_blob.py
@@ -5,14 +5,20 @@ from ..services.blob_storage import upload_to_blob_storage, list_blobs
 
 router = APIRouter()
 
+
+@router.get("/")
+def read_root():
+    return {"message": "Welcome to the Vercel Blob API"}
+
+
 @router.get("/files", dependencies=[Depends(verify_token)])
 async def list_files():
-    return list_blobs()
+    return JSONResponse(status_code=200, content={"data": list_blobs()})
 
 
 @router.post("/upload", dependencies=[Depends(verify_token)])
 async def upload(file: UploadFile = File(...)):
     contents = await file.read()
-    url, path = upload_to_blob_storage(file.filename, contents)
-    
-    return JSONResponse(status_code=200, content={"url": url, "path": path})
+    url, _ = upload_to_blob_storage(file.filename, contents)
+
+    return JSONResponse(status_code=200, content={"url": url})
