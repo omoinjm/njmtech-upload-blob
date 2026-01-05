f1e85e259e3fe6ef0092261cd9d93d6b790d6743
omoinjm <njmcloud@gmail.com>
2026-01-05 16:12:21 +0200

    yizo lezi x2

--- api/services/blob_storage.py
+++ api/services/blob_storage.py
@@ -2,23 +2,24 @@ import vercel_blob
 from werkzeug.utils import secure_filename
 from datetime import datetime
 
+
 def upload_to_blob_storage(filename: str, contents: bytes) -> tuple[str, str]:
     sanitized_filename = secure_filename(filename)
     now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    path = f"njmtech-blob-api/{now}/{sanitized_filename}"
+    path = f"njmtech-blob-api/{sanitized_filename}"
     blob_result = vercel_blob.put(path, contents)
-    return blob_result['url'], path
+    return blob_result["url"], path
+
 
 def list_blobs():
     result = vercel_blob.list()
     # Filter by prefix manually after getting results
     formatted_blobs = []
-    for blob in result.get('blobs', []):
+    for blob in result.get("blobs", []):
         # Filter by prefix manually after getting results
-        pathname = blob.get('pathname', '')
-        if pathname.startswith('njmtech-blob-api/'):
-            formatted_blobs.append({
-                "url": blob.get('url'),
-                "path": blob.get('pathname')
-         })
+        pathname = blob.get("pathname", "")
+        if pathname.startswith("njmtech-blob-api/"):
+            formatted_blobs.append(
+                {"url": blob.get("url"), "path": blob.get("pathname")}
+            )
     return formatted_blobs
