250f32b325c1bfaaaaabca33870fd76146db6306
omoinjm <njmcloud@gmail.com>
2026-01-11 12:54:38 +0200

    hamba nabo

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -4,7 +4,6 @@ set -e
 TRANSCRIPT_FILE="/tmp/njmtech-yt-transcribe/transcript.txt"
 API_TOKEN="WUHQpsClkPo9GdmUxu7jFhPAWQ1SEdiyR"
 
-curl -X POST \
+curl -X POST "http://localhost:8008/api/v1/blob/upload?blob_path=tests" \
   -H "Authorization: Bearer $API_TOKEN" \
-  -F "file=@${TRANSCRIPT_FILE}" \
-  http://localhost:8008/api/v1/blob/upload | jq
+  -F "file=@${TRANSCRIPT_FILE}" | jq
